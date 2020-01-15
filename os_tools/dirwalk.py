ffe54fefd9a5bd8ea299ab7875c51c3896bbf8f7
grahamkesley <grahamkesley@gmail.com>
2020-01-15 15:28:48 +0000

    Added colours for readability

--- os_tools/dirwalk.py
+++ os_tools/dirwalk.py
@@ -3,10 +3,20 @@ import os
 
 os.chdir("/Users/grahamkesley/projects/")
 
+class Bcolors:
+    """ANSI colours for terminal printing"""
+    DIRECTORIES = '\033[95m'
+    FILES = '\033[94m'
+    LABELS = '\033[93m'
+    ENDC = '\033[0m'
+
+
 for root, dirs, files in os.walk(".", topdown=False):
-    print("In folder: " + root)
+    print(Bcolors.LABELS + "In folder: " + root + Bcolors.ENDC)
     for name in dirs:
-        print("Directories: " + os.path.join(root, name))
+        print(Bcolors.DIRECTORIES + "Directories: " + Bcolors.ENDC
+              + os.path.join(root, name))
     for name in files:
-        print("Files: " + os.path.join(root, name))
+        print(Bcolors.FILES + "Files: " + Bcolors.ENDC
+              + os.path.join(root, name))
     print("---------\n")
